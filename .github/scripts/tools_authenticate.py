820fe29219da276a7a3e54a6af7668020d5875ef
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-07-25 17:20:42 +0200

    Update tools_authenticate.py

--- .github/scripts/tools_authenticate.py
+++ .github/scripts/tools_authenticate.py
@@ -10,6 +10,8 @@ import sys
 # Mapping file: the file to match against to verify user, team and path. If none, default is used 
 
 # matching file configuration
+# TODO DO -> Move to an external config file
+default_saving_path = 'previsioni'
 default_mapping_folder = 'assests'
 default_mapping_file = 'authenticate-mapping.json'
 
@@ -20,27 +22,60 @@ class Authenticator () :
     def __init__(self, user, changes):
         self.user = user
         self.changes = changes
-        self.mappings = _defaultMappings()
+        self.mappings = self._defaultMappings()
 
         self.team = None
         self.models = None
 
 
-    #
-    #
-    def verifyPaths (self):
-
-        for model in self.models :
-            # Pattern that matches a forecast file added to the data-processed folder.
-            # Test this regex usiing this link: https://regex101.com/r/wmajJA/1
-            forecast_fname_matching = re.compile(r"^previsioni/(.+)/\d\d\d\d-\d\d.csv$")
+    def authenticate (self): 
+        # verify user
+        if not self._authenticateUser():
+            # handle invalid user
+            return False
         
-            invalid_forcast_names = [filename for filename in self.changes if forecast_fname_matching.match(filename) is None]
+        if not self._verifyPaths():
+            # handle invalid paths
+            return False
+        
+        # everything look ok from an authentication point of view
+        return True
 
-    
-    # 
     #
-    def authenticateUser (self):
+    #
+    def _verifyPaths (self):
+
+        matching_list = []
+        for model in self.models :
+            matching_list.append(os.path.join(default_saving_path, self.team + '_' + model))
+
+        invalid_forcast_paths = [changed_file for changed_file in self.changes if not os.path.split(changed_file)[0] in matching_list]
+        if invalid_forcast_paths:
+            # handle trying to save an unauthorised path 
+            print ("trying to save in unauthorised path")
+            return False
+        
+        print ("Paths look ok, check file name")
+
+        # Pattern that matches a forecast file added to the data-processed folder.
+        # Test this regex usiing this link: https://regex101.com/r/wmajJA/1
+        forecast_fname_matching = re.compile(r"^previsioni/(.+)/\d\d\d\d_\d\d.csv$")    
+        invalid_forcast_names = [changed_file for changed_file in self.changes if forecast_fname_matching.match(changed_file) is None]
+
+        if invalid_forcast_names:
+            # handle trying to save an unauthorised path 
+            print ("trying to save with an invalid file name")
+            return False
+
+        print ("Names look ok, go on")
+
+        return True
+    
+
+    # 
+    # Verify that the actor of the PR is currently in the authorised db and 
+    # map it back to its Team and Models
+    def _authenticateUser (self):
         j_in = None
         
         # load mappings file
@@ -63,22 +98,14 @@ class Authenticator () :
 
         # nothing found 
         return False
-
-
     
     def _defaultMappings (self):
         return os.path.join(os.path.dirname(__file__), '..', default_mapping_folder, default_mapping_file)
 
 
-
-
-    
-
-
-
 #
-def run (jsonInputFile):
-    
+def run ():
+
     actor = os.getenv("calling_actor")
     changes = os.getenv("changed_files")
 
@@ -90,13 +117,14 @@ def run (jsonInputFile):
     print ("### Changed List: {}".format(changes))
 
     authenticateObj = Authenticator(actor, changes)
-    authenticateObj.authenticateUser()
+    authenticateObj.authenticate()
+
+    return True
     
 
 
 
 if __name__ == "__main__":
     print ("### Testing tools_authenticate script")
+
     passed = run()
-    
-    return passed
