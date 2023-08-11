0b842b613625147c79959d4e61401278ec4328dd
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-08-11 18:45:33 +0200

    Update validate.py

--- .github/scripts/forecast_validation/validate.py
+++ .github/scripts/forecast_validation/validate.py
@@ -1,1 +1,28 @@
 # main
+import os
+import re
+import json
+import sys
+
+import validate_forecasts as v
+
+
+def run ():
+    print (">>>>>>>>> Running validation main")
+
+    env_file = os.getenv('GITHUB_OUTPUT')    
+    to_validate = os.getenv("changed_files")
+
+    # test only
+    if not isinstance(toValidate, (list, tuple)):
+        print ("to validate is not a list")
+        toValidate = [toValidate]
+
+    for elem in toValidate:
+        print ("Validating {}".format(elem))
+        v.validate_csv_files("influcast_flu_forecast", elem)
+    
+
+if __name__ == "__main__":
+    print ("### Testing tools_validate script")
+    run()
