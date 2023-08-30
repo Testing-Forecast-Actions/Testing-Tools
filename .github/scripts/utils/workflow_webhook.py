ec890c7e05f69a62fca6d22f9c2a8fb21f53d0b7
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-08-30 22:37:50 +0200

    Update workflow_webhook.py

--- .github/scripts/utils/workflow_webhook.py
+++ .github/scripts/utils/workflow_webhook.py
@@ -9,16 +9,20 @@ import hashlib
 
 
 class BodyDigestSignature(object):
-    def __init__(self, secret, header='Sign', algorithm=hashlib.sha512):
+    def __init__(self, secret, header='Sign', algorithm=hashlib.sha256):
         self.secret = secret
         self.header = header
         self.algorithm = algorithm
 
     def __call__(self, request):
         body = request.body
-        if not isinstance(body, bytes):   # Python 3
-            body = body.encode('latin1')  # standard encoding for HTTP
-        signature = hmac.new(self.secret, body, digestmod=self.algorithm)
+
+        print ("Body: {}".format(body))
+
+        if not isinstance(body, bytes):  
+            body = body.encode('utf-8')  
+
+        signature = hmac.new(self.secret.encode('utf-8'), body, digestmod=self.algorithm)
         request.headers[self.header] = signature.hexdigest()
         return request
 
@@ -33,32 +37,35 @@ class Sender () :
   def send (self, payload, signature):
     # x-hub-signature-256" in request.headers
 
-    r = requests.post(self.webhook_url, json=payload, headers = headers, auth=BodyDigestSignature(secret))
-    
-    print(f"Status Code: {r.status_code}, Response: {r.json()}")
+    r = requests.post(self.webhook_url, json=payload, auth=BodyDigestSignature(secret))
+    print ("Response: {}".format(r))
+    # print(f"Status Code: {r.status_code}, Response: {r.json()}")
 
 
 #
 def run ():
-  # get env parameters
-  env_file = os.getenv('GITHUB_OUTPUT')
     
-  wh_url = os.getenv("webhook_url")
-  wh_secret = os.getenv("webhook_secret")
-  custom_json_data = os.getenv("data")
-
-  # debug only, to be removed
-  print ("### Url: {}".format(wh_url))
-  print ("### Secret: {}".format(wh_secret))
-  print ("### Data: {}".format(custom_json_data))
-  # debug only, to be removed
-
-
-  if wh_url is None or wh_secret is None or custom_json_data is None:
+    # get env parameters
+    env_file = os.getenv('GITHUB_OUTPUT')
+    
+    wh_url = os.getenv("webhook_url")
+    wh_secret = os.getenv("webhook_secret")
+    custom_json_data = os.getenv("data")
+    
+    # debug only, to be removed
+    print ("### Url: {}".format(wh_url))
+    print ("### Secret: {}".format(wh_secret))
+    print ("### Data: {}".format(custom_json_data))
+    # debug only, to be removed
+    
+    
+    if wh_url is None or wh_secret is None or custom_json_data is None:
       return False
-  
-
-  return True
+    
+    sender_obj = Sender (wh_url)
+    sender_obj.send(custom_json_data, wh_secret)
+    
+    return True
 
 
 
