b00c8b284566b3f9f6151f364f3b31e100e10c98
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-09-01 17:03:48 +0200

    Update validate.py

--- .github/scripts/forecast_validation/validate.py
+++ .github/scripts/forecast_validation/validate.py
@@ -10,8 +10,8 @@ import validate_forecasts as v
 def run ():
     print (">>>>>>>>> Running validation main")
 
-    res_data = {}
-    res_data['models'] = {}
+    # res_data = {}
+    # res_data['models'] = {}
 
 
     env_file = os.getenv('GITHUB_OUTPUT')    
@@ -23,16 +23,16 @@ def run ():
         print ("Validating {}".format(elem))
         v.validate_csv_files("influcast_flu_forecast", elem)
         
-        team_model = tuple(os.path.basename(os.path.split(elem)[0]).split('_'))
+        # team_model = tuple(os.path.basename(os.path.split(elem)[0]).split('_'))
 
-        # add the team. It is only one
-        if team_model[0] not in res_data:
-            res_data['team'] = team_model[0]
+        # # add the team. It is only one
+        # if team_model[0] not in res_data:
+        #     res_data['team'] = team_model[0]
 
-        if team_model[1] not in res_data['models'] :
-           res_data['models'][team_model[1]] = [elem]
-        else:
-           res_data['models'][team_model[1]].append(elem)    
+        # if team_model[1] not in res_data['models'] :
+        #    res_data['models'][team_model[1]] = [elem]
+        # else:
+        #    res_data['models'][team_model[1]].append(elem)    
 
     
     validated = True
