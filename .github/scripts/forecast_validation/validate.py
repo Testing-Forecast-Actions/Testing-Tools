474555a9a51ca81317acfb17925c87bbdb6d32d3
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-09-01 16:09:17 +0200

    Update validate.py

--- .github/scripts/forecast_validation/validate.py
+++ .github/scripts/forecast_validation/validate.py
@@ -10,6 +10,10 @@ import validate_forecasts as v
 def run ():
     print (">>>>>>>>> Running validation main")
 
+    res_data = {}
+    res_data['models'] = {}
+
+
     env_file = os.getenv('GITHUB_OUTPUT')    
     to_validate = os.getenv("changed_files")
 
@@ -18,11 +22,25 @@ def run ():
     for elem in to_validate:
         print ("Validating {}".format(elem))
         v.validate_csv_files("influcast_flu_forecast", elem)
+        
+        team_model = tuple(os.path.basename(os.path.split(elem)[0]).split('_'))
 
+        # add the team. It is only one
+        if team_model[0] not in res_data:
+            res_data['team'] = team_model[0]
+
+        if team_model[1] not in res_data['models'] :
+           res_data['models'][team_model[1]] = [change]
+        else:
+           res_data['models'][team_model[1]].append(change)    
+
+    
     validated = True
+    
     
     with open(env_file, "a") as outenv:
         outenv.write ("validation={}".format(validated))
+        outenv.write ("output_data={}".format(res_data))
 
 
 if __name__ == "__main__":
