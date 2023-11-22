ab4698e375e18e9ef709c66bc1391bbf18cd0090
M-7th <paolo.milano@isi.it>
2023-11-22 14:50:36 +0100

    test fix

--- .github/scripts/forecast_validation/validate.py
+++ .github/scripts/forecast_validation/validate.py
@@ -8,10 +8,8 @@ import validate_forecasts as v
 
 
 def run ():
-    print (">>>>>>>>> Running validation main")
 
-    # res_data = {}
-    # res_data['models'] = {}
+    validated = True
 
 
     env_file = os.getenv('GITHUB_OUTPUT')    
@@ -21,29 +19,23 @@ def run ():
 
     for elem in to_validate:
         print ("Validating {}".format(elem))
-        v.validate_csv_files("influcast_flu_forecast", elem)
-        
-        # team_model = tuple(os.path.basename(os.path.split(elem)[0]).split('_'))
-
-        # # add the team. It is only one
-        # if team_model[0] not in res_data:
-        #     res_data['team'] = team_model[0]
-
-        # if team_model[1] not in res_data['models'] :
-        #    res_data['models'][team_model[1]] = [elem]
-        # else:
-        #    res_data['models'][team_model[1]].append(elem)    
-
+        try:
+            v.validate_csv_files("influcast_flu_forecast", elem)
+        except:
+            validated = False
+            break
     
-    validated = True
     
     
     with open(env_file, "a") as outenv:
         print(f"Writing to out: validate: {validated}")
         outenv.write (f"validation={validated}")
-        # outenv.write ("output_data={}".format(str(res_data)))
 
+    return validated
 
 if __name__ == "__main__":
     print ("### Testing tools_validate script")
-    run()
+
+    valid_run = run()
+    if not valid_run:
+        sys.exit(1)
