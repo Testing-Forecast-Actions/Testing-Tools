ab4009050c47d9e28fb496da0a84ef1574830225
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-12-15 16:55:40 +0100

    Update test_code.py

--- code/test_code.py
+++ code/test_code.py
@@ -6,13 +6,7 @@ if __name__ == "__main__" :
   input_variable = os.environ['INPUT_STORE']
   print (f"Invar is {input_variable} ")
 
-  isString = isinstance(input_variable, str)
-  if isString:
-    print ('Var is string')
-  else:
-    print ('Var is not a string')
-
-  if input_variable: 
+  if input_variable == 'true': 
     print ('Acting as if it is true')
   else:
     print ('Acting as if it is false') 
