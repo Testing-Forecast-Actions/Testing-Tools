f6c67f8d45989d6feffa0fe2a0deaa31194f1fcd
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-12-15 15:29:46 +0100

    Update test_code.py

--- code/test_code.py
+++ code/test_code.py
@@ -5,3 +5,9 @@ if __name__ == "__main__" :
   
   input_variable = os.environ['INPUT_STORE']
   print (f"Invar is {input_variable} ")
+
+
+  if input_variable: 
+    print ('Acting as if it is true')
+  else:
+    print ('Acting as if it is false') 
