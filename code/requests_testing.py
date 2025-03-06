e355510bc2e2f78096a6ec99356c0ed05cebfe8f
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2025-03-06 14:24:06 +0100

    Update requests_testing.py

--- code/requests_testing.py
+++ code/requests_testing.py
@@ -1,7 +1,7 @@
 import requests
 
 print ("Python Requests - Code test start")
-url = 'https://djgleam.isi.it/sleepy/30/'
+url = 'https://djgleam.isi.it/sleepy/390/'
 x = requests.get(url)
 
 print("Python Requests - Code test completed")
