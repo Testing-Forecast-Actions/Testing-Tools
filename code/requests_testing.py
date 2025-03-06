391ab4ddba7c12e22ba99a71ce3a97dd204a5874
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2025-03-06 16:21:29 +0100

    Update requests_testing.py

--- code/requests_testing.py
+++ code/requests_testing.py
@@ -3,7 +3,8 @@ import requests
 print ("Python Requests - Code test start")
 url = 'https://djgleam.isi.it/sleepy/400/'
 test_session = requests.Session()
-x = test_session.get(url)
+test_session.headers.update({ "Connection":"keep-alive" })
+x = test_session.get(url, timeout=500)
 # x = requests.get(url)
 
 print("Python Requests - Code test completed")
