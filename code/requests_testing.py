e9e70b18cf34f73b866bcc3b1be43d8dd207fa51
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2025-03-07 17:47:55 +0100

    Update requests_testing.py

--- code/requests_testing.py
+++ code/requests_testing.py
@@ -2,15 +2,16 @@ import requests
 import os
 
 t_out = os.getenv("time_out")
-
+rt_out = os.getenv("r_time_out")
+r_time_out
 print ("Python Requests - Code test start")
 
-url = 'https://djgleam.isi.it/sleepy/' + t_out + '/'
+url = 'https://djgleam.isi.it/sleepy/' + rt_out + '/'
 
-print(f"calling url: {url}")
+print(f"calling url: {url} with timeout: {t_out}")
 
 try:
-  response = requests.get(url, timeout=330)
+  response = requests.get(url, timeout=t_out)
   
   print("Response headers:", response.headers)
   print(response.json())
