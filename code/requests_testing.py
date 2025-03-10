4c7b65dbc505f8adca3667dae723f8ae63a3f91e
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2025-03-10 13:38:06 +0100

    Update requests_testing.py

--- code/requests_testing.py
+++ code/requests_testing.py
@@ -8,12 +8,13 @@ rt_out = os.getenv("r_time_out")
 print ("Python Requests - Code test start")
 
 # url = 'https://djgleam.isi.it/sleepy/' + rt_out + '/'
-url = 'https://httpbin.org/delay/'+ rt_out
+url = 'http://chimera.isi.it/'
+data = {"delay": rt_out}
 
 print(f"calling url: {url} with timeout: {t_out}")
 
 try:
-  response = requests.get(url, timeout=t_out)
+  response = requests.post(url, json=data, timeout=t_out)
   
   print("Response headers:", response.headers)
   print(response.json())
