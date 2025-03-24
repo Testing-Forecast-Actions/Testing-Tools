a1228c3e3cc26e19c5e3216db9b2991a2e46c70e
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2025-03-24 18:46:04 +0100

    Update extract_emails.py

--- code/extract_emails.py
+++ code/extract_emails.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import yaml
+import argparse
 
 def extract_emails_from_yml(folder_path, excluded_files):
-    emails = []
+    emails = set() # use a set to avoid duplicates
 
     for filename in os.listdir(folder_path):
         if not filename.endswith('.yml') or filename in excluded_files:
@@ -17,19 +18,27 @@ def extract_emails_from_yml(folder_path, excluded_files):
                 for contributor in contributors:
                     email = contributor.get('email')
                     if email:
-                        emails.append(email)
+                        emails.add(email.strip())
         except Exception as e:
             print(f"Errore nella lettura del file {filename}: {e}")
 
-    return emails
+    return sorted(emails)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python script.py <folder_path> <excluded_file1> <excluded_file2> ...")
-        sys.exit(1)
 
-    folder = sys.argv[1]
-    excluded = sys.argv[2:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder_path')
+    parser.add_argument('--exclusions')
 
-    emails = extract_emails_from_yml(folder, excluded)
-    print("\n".join(emails))
+    args = parser.parse_args()
+
+    print (f"Folder: {args.folder_path},\nexclusions: {args.exclusions}")
+
+    folder = args.folder_path
+    excluded = args.exclusions
+    
+
+    emails = extract_emails_from_yml(folder, excluded.split())
+    print("\n\nExtracted: \n".join(emails))
+
+    print(f"\nTotale email uniche trovate: {len(emails)}")
